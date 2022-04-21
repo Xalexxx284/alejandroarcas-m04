d1924ba354c614e7092247355a2f44e2bb0e5542
Xalexxx284 <94836307+Xalexxx284@users.noreply.github.com>
2022-04-21 16:09:12 +0200

    Create DomProject.js

---
+++ JavaScript/Ej5Dom/DomProject.js
@@ -0,0 +1,17 @@
+// 1. Agafa el fitxer de base DOMExercici1.html. Crea un fitxer .js i fes-lo enllaçar amb el fitxer html. Fes que el fitxer .js modifiqui l’estil de paràgraf tot utilitzant el botó. 
+// 2. Agafa el fitxer de base DOMExercici2.html. Crea un fitxer .js i fes-lo enllaçar amb el fitxer html. Fes que el fitxer .js prengui els valors del “nom” i “cognom” i els mostri per la consola del navegador. 
+// 3. Crea un fitxer html complert amb diversos paràgrafs. i un botó. Crea un fitxer .js i fes-lo enllaçar amb el fitxer html. Fes que en fer clic en el botó es modifiqui el color 
+//    de fons dels paràgrafs. Fes que els paràgrafs no tinguin id. 
+// 4. Agafa el fitxer de base DOMExercici4.html. Crea un fitxer .js i fes-lo enllaçar amb el fitxer html. Fes que el fitxer .js prengui els atributs definits en l’enllaç html i els mostri per la consola del navegador. 
+// 5. Agafa el fitxer de base DOMExercici5.html. Crea un fitxer .js i fes-lo enllaçar amb el fitxer html. Fes que el fitxer .js afegeixi una fila nova al principi de la taula. 
+//    a. Repeteix l’exercici tot afegint la fila al final de la taula. 
+// 6. Modifica l’exercici anterior per a que el codi .js modifiqui el valor d’una cel·la de la taula escollit arbitràriament. 
+//    a. En un primer pas fes que els valors de la fila i cel·la estiguin “hard-coded” en el codi. 
+//    b. En un segon pas fes que els valors de la fila i cel·la es llegeixin des de l’usuari via html. (Hauràs de modificar l’html). 
+// 7. Crea un .html i un .js en el qual es demani amb la comanda prompt (pots 
+//    reutilitzar-la) el número de files i columnes que ha de tenir una taula. Tot seguit modifica una taula buida existent en l’html per a que tingui les fileres i columnes demanades. 
+// 8. Crea un html amb un dropdownlist (amb diverses opcions) i un botó. Enllaça’l amb un javascript i fes que el botó elimini el valor seleccionat del dropdownlist. 
+// 9. Crea un html amb un dropdownlist (amb diverses opcions) i un botó. Enllaça’l amb un javascript i fes que el botó dispari un event “alert” on surti el nombre i els valors 
+//    dels elements del dropdownlist. 
+// 10. Crea un formulari que donat l’alçada d’un cilindre et retorni el seu volum. Cal prendre el valor de l’usuari per html i mostrar al clicar el botó el resultat en l’html. 
+// 11. Agafa l’html d’exemple i modifica’l per a que les paraules resaltades modifiquin el seu tamany i color quan el ratolí passi per sobre de l’enllaç definit per això.
