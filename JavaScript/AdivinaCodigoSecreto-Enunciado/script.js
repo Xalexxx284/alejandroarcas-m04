b72c0e26bca4d770ad2deb415ade3a58c76b0233
Xalexxx284 <94836307+Xalexxx284@users.noreply.github.com>
2022-05-02 12:15:11 +0200

    Update script.js

--- JavaScript/AdivinaCodigoSecreto-Enunciado/script.js
+++ JavaScript/AdivinaCodigoSecreto-Enunciado/script.js
@@ -4,11 +4,10 @@ const maxIntento = 8;
 /*1. Genera una constante CODIGO_SECRETO de tipo array de 5 número aleatorios entre 0 y 9 usando la libreria Math.random();*/
 function codigoSecreto() {
     for (let i = 0; i < 5; i++) {
-        codigo[i] = Math.floor((Math.random() * 10));
+        let newNumber = Math.floor(Math.random() * 10 + 1);
+        codigo.push(newNumber);  
     }
+    return codigo
 }
-
 codigoSecreto();
-
-
 
