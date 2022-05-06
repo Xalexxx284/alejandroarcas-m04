34e6b1dd63e0267eaa6bec07b494563efa308924
alejandro <alejandro.arcas.7e5@itb.cat>
2022-05-06 09:58:17 +0200

    dgdg

--- JavaScript/AdivinaCodigoSecreto-Enunciado/script.js
+++ JavaScript/AdivinaCodigoSecreto-Enunciado/script.js
@@ -1,13 +1,46 @@
-const codigo = [];
-const maxIntento = 8;
+// -------------------------------------------------------------- DECLARACION DE VARIABLES -------------------------------------------------------------- //
+const code = [];
+const maxTry = 5;
 
-/*1. Genera una constante CODIGO_SECRETO de tipo array de 5 número aleatorios entre 0 y 9 usando la libreria Math.random();*/
-function codigoSecreto() {
+// ------------------------------------------------------------------- SECCION CODIGO ------------------------------------------------------------------- //
+
+/* 1. Inicialmente hay * en cada celda, pero cuando haya acabado el programa saldrá el número.*/
+
+function secretCode(code) {
     for (let i = 0; i < 5; i++) {
         let newNumber = Math.floor(Math.random() * 10 + 1);
-        codigo.push(newNumber);  
+        code.push(newNumber);  
     }
-    return codigo
+    return code
 }
-codigoSecreto();
+console.log(secretCode(code))
 
+// ------------------------------------------------------------------ SECCION COMPROBAR ----------------------------------------------------------------- //
+
+/* 1. Generar celdas de resultado */
+
+function generateResultCells(){
+    const divRow = document.getElementById("Result");
+    let createElement = document.createElement("div");
+    
+    createElement.className = "rowResult"
+
+    createELement.addEventListener('click')
+    
+
+    document.divRow.appendChild(createElement);
+
+}
+
+
+/* 2. Input dónde el usuario pondrá los 5 dígitos. Inicialmente hay 01234, pero se borrará y cogerá el foco en cada iteración. */
+
+/* 3. Botón que lanza la función que verifica si la propuesta indicada por el usuario es válida o no. */
+
+
+
+// -------------------------------------------------------------------- SECCION INFO -------------------------------------------------------------------- //
+
+
+
+// ------------------------------------------------------------------- SECCION RESULT ------------------------------------------------------------------- //
